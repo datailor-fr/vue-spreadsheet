e33f6801fdb7ae060809e328054d0f8b8f54ffa4
m.sorce <m.sorce@datailor.fr>
2025-07-24 15:14:29 +0200

    Fix JS conf

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,14 @@
-// vue.config.js
-module.exports = {
-  css: { extract: false },
-};
+import { fileURLToPath, URL } from 'node:url'
+
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [vue()],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
+  }
+})
